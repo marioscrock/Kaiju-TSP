7fbc6a6d3ea811e4d681a5183d2569feb68541b2
mario_scrock <mario.scrocca@gmail.com>
2018-10-02 21:52:02 +0200

    Add socket client client.go

---
+++ client.go
@@ -0,0 +1,20 @@
+package main
+
+import "net"
+import "fmt"
+import "bufio"
+import "os"
+
+func main() {
+
+  // connect to this socket
+  conn, _ := net.Dial("tcp", "127.0.0.1:9876")
+  for { 
+    // read in input from stdin
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("Text to send: ")
+    text, _ := reader.ReadString('\n')
+    // send to socket
+    fmt.Fprintf(conn, text)
+  }
+}
