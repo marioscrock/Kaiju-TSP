92adce3509edc64c30c0fbef5e0033f2e9f1cdc6
marioscrock <mario.scrocca@gmail.com>
2019-03-04 15:27:15 +0100

    Update Metric.java

--- src/main/java/eventsocket/Metric.java
+++ src/main/java/eventsocket/Metric.java
@@ -15,15 +15,15 @@ public class Metric implements Serializable {
 	private static final long serialVersionUID = -1795026022912504200L;
 	
 	public String name;
-	public Map<String, String> fields = new HashMap<String, String>();
+	public Map<String, Float> fields = new HashMap<String, Float>();
 	public Long timestamp;
 	public Map<String, String> tags = new HashMap<String, String>();
 	
 	
-	public Map<String, String> getFields() {
+	public Map<String, Float> getFields() {
 		return fields;
 	}
-	public void setFields(Map<String, String> fields) {
+	public void setFields(Map<String, Float> fields) {
 		this.fields = fields;
 	}
 	public String getName() {
